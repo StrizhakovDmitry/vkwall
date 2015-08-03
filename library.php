df1a07996f79d3d1d4e6509ae25dfe2391f55d2b
Dmitry Strizhakov <strizhakovdv@gmail.com>
2015-08-03 21:23:27 +0300

    исправлена ошибка верстки -  добавлено свойство display-block к блокам содержания постов

--- library.php
+++ library.php
@@ -10,7 +10,7 @@ class library {
 	public $postsQuantity = 50; //количество постов со стены
 	public $offset = 0; //с какого поста наичнать 
 	const CLIENT_ID = 5015617; //ID приложение 
-	const CLIENT_SECRET = '*****'; //секретный код приложения, в целях безопасности убрал из кода
+	const CLIENT_SECRET = 'EqMZZhD4CntAte8jlTGO'; //секретный код приложения, в целях безопасности убрал из кода
 	const PROFILE_VK_PATH = 'https://vk.com/id'; 
 	public $transArr = array(); 
 	public $obj_profiles;
@@ -57,10 +57,12 @@ class library {
 		//echo '<pre>';var_dump($obj_response -> response->wall[1]);exit;
 		//echo '<pre>';var_dump($obj_response -> response);exit;
 		
+		$html.='<div class="wallСontainer">';
 		for ($i = 1; $i < ($this -> postsQuantity + 1); $i++) {
 			$obj_wall = $obj_response -> response->wall[$i];
 			$html .= $this -> postObjToHtml($obj_wall,$template);
 		}
+		$html.='</div>';
 		return $html;
 	}
 
